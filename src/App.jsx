e41d932d89ca94b69686998e160daa2e209c7b73
Cynthia <s.cynthia972@gmail.com>
2024-01-08 14:17:15 +0100

    Début création routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/login'
+import Admin from './pages/admin'
+import User from './pages/user'
+import CreateUser from './pages/createUser'
 
 
-function App() {
+    function App() {
 
-  return (
-    <>
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<Login/>} />
+                    <Route path={'/admin'} element={<Admin/>} />
+                    <Route path={'/admin/:firmname'} element={<CreateUser/>} />
+                    <Route path={'/user/:firmname'} element={<User />} />
+                </Routes>
+            </BrowserRouter>
 
+        </>
+    )
+    }
 
-    </>
-  )
-}
-
-export default App
+    export default App
---
+++ src/pages/createUser.jsx
@@ -0,0 +1,7 @@
+export default function CreateUser() {
+    return(
+        <>
+            <h1>Page creation profil utilisateur</h1>
+        </>
+    )
+}
