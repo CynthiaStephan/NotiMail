06b2bc9b589bfee0947cae9d70837228be9e1c3d
Cynthia <s.cynthia972@gmail.com>
2024-01-09 12:29:44 +0100

    Modal notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,16 +1,30 @@
+import './Notification.css'
 import { FaCheck } from "react-icons/fa6";
 import { FaXmark } from "react-icons/fa6";
+import { IconContext } from "react-icons";
 export default function Notification() {
 
     return(
         <>
-            <div className="notifModal">
-                <h2>Confimer la réception  du courrier</h2>
+            <IconContext.Provider value={{ color: "white", className: "notif-button", size:"30px" }}>
+            <div className='notification-modal'>
                 <div>
-                    <button><FaXmark /></button>
-                    <button><FaCheck /></button>
+                    <h2>Confimer la réception du courrier :</h2>
+                    <p></p>
+                </div>
+                
+                <div className='notif-button-frame'>
+
+                    <button className='notif-cancel-button'>
+                        <FaXmark />
+                    </button>
+                    <button className='notif-validate-button'>
+                        <FaCheck />
+                    </button>
+
                 </div>
             </div>
+            </IconContext.Provider>
         </>
     )
 }
