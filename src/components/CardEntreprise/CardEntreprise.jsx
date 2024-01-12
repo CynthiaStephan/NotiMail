8d4acc9b5179c386c3088af8202491e81d18fd2e
Cynthia <s.cynthia972@gmail.com>
2024-01-12 09:55:40 +0100

    kojdfzoikjjfze

--- src/components/CardEntreprise/CardEntreprise.jsx
+++ src/components/CardEntreprise/CardEntreprise.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { FaPenToSquare } from "react-icons/fa6";
+import { FaPenToSquare, FaPhone, FaAt } from "react-icons/fa6";
 import { IconContext } from "react-icons";
-import { Checkbox } from "evergreen-ui";
+import { Checkbox, Spinner } from "evergreen-ui";
+
+
 
 export default function CardEntreprise(){
 
@@ -10,35 +12,99 @@ export default function CardEntreprise(){
     const toggleVisibility = () => {
       setVisible(!visible);
     };
-    /* Checkbox */
 
+    /* Vérifier que l'information a bien été récupéré*/
+    const [load, setLoad] = useState(true);
+    
+    /* Checkbox */
     const [checked, setChecked] = React.useState(true)
+
+    /* Fetch pour récupérer les informations d'entreprise */
+    const [companies, setCompanies] = useState([])
+
+    useEffect(() => {
+        
+        const requestBody = {
+                 };
+
+        fetch('http://51.83.69.229:3000/api/users/gestionEntreprise', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la récupération des données');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setCompanies(data)
+            setLoad(false);
+            console.log(data)
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+            setLoad(false);
+        });
+    }, []);
+    
+    
     
     return(
         <>
-            <div>
+            {/* Je m'assure que l'information est bien chargé */}
+            {load ? (
+                <Spinner size={24} />
+            ) : (
+            companies && companies.map((items, index) => (
 
-                <div>
-                    <div className="pastille"></div>
-                    <IconContext.Provider value={{ color: "white", className: "card-button", size:"24px" }}>
-                        <FaPenToSquare />
-                    </IconContext.Provider>
+                <div className="companie-card" key={index}>
+
+                    <div className="top-companie-card">
+                        <div>
+                            <div className="pastille"></div>
+                            <IconContext.Provider value={{ color: "white", className: "card-button", size:"24px" }}>
+                                <FaPenToSquare />
+                            </IconContext.Provider>
+                        </div>
+
+                        <div  onClick={toggleVisibility}>
+                            <h2>Zenith Dynamics</h2>
+                            <p>Marion P.</p>
+                            <p>19/09/2023</p>
+                        </div>
+
+                        <div>
+                            <Checkbox
+                            checked={checked}
+                            onChange={e => setChecked(e.target.checked)}
+                            />
+                        </div>
+
+                    </div>
+
+                    {visible && (
+                        <IconContext.Provider value={{ color: "white", className: "", size:"30px" }}>
+                        <div className="bottom-companie-card">
+                            <div>
+                                <FaAt />
+                                <a href=""></a>
+                            </div>
+
+                            <div>
+                                <FaPhone />
+                                <a href=""></a>
+                            </div>
+                        </div>
+                        </IconContext.Provider>
+                    )}
                 </div>
+                ))
+            )}
 
-                <div>
-                    <h2>Zenith Dynamics</h2>
-                    <p>Marion P.</p>
-                    <p>19/09/2023</p>
-                </div>
-
-                <div>
-                    <Checkbox
-                    checked={checked}
-                    onChange={e => setChecked(e.target.checked)}
-                    />
-                </div>
-
-            </div>
 
             {/* <p onClick={toggleVisibility}>NovaSphere Solutions</p>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,54 +1,9 @@
-// import { useEffect, useState } from 'react'
-// import './Header.css'
-
-// export default function Header(){
-
-//     const[firmName, setFirmName] = useState('')
-
-//     useEffect(() => {
-//         fetch('http://51.83.69.229:3000/api/users/gestionEntreprise')
-            
-
-//         .then(response => {
-//             // Vérification de la réponse HTTP (status 200 indique une réponse réussie)
-//             if (!response.ok) {
-//               throw new Error('Erreur lors de la récupération des données');
-//             }
-//             // Conversion de la réponse en format JSON et retourne la promesse
-//             return response.json();
-//           })
-//         .then((data) => {
-//             setFirmName(data)
-//             console.log(data)
-//         })
-//         .catch(error => console.error('Erreur:', error)); // Gestion des erreurs.
-
-//     },[]);
-    
-
-//     // Récupérer qui est l'utilisateur, et en fonction afficher dans P l'utilisateur
-//     return(
-//         <>
-//             <div>
-//                 <img src="./Logo.png" alt="Logo NotiMail" />
-//                 <div>
-//                     <p>Nom du compte</p>
-//                     <button type="button" id="logoutButton" aria-label="Se déconnecter">
-//                         Deconnexion
-//                     </button>
-//                 </div>
-//             </div>
-            
-//         </>
-//     )
-// }
-
 import { useEffect, useState } from 'react'
 import './Header.css'
 
 export default function Header() {
 
-    const [firmName, setFirmName] = useState([])
+    const [companies, setCompanies] = useState([])
 
     useEffect(() => {
         
@@ -69,7 +24,7 @@ export default function Header() {
             return response.json();
         })
         .then((data) => {
-            setFirmName(data)
+            setCompanies(data)
             console.log(data)
         })
         .catch(error => console.error('Erreur:', error));
@@ -82,8 +37,8 @@ export default function Header() {
                 <div>
                     {/* <p>{firmName[0].firm_name}</p> */}
                     
-                    {firmName.length > 0 ? (
-                    <p>{firmName[0].firm_name}</p>
+                    {companies.length > 0 ? (
+                    <p>{companies[0].firm_name}</p>
                     ) : (
                     <p>Chargement en cours...</p>
                     )}
